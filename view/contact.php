d6e6328f764c0fe298da33fc3e6aea95700d1341
sujan0238 <sujan.chauhan@deerwalk.edu.np>
2017-01-18 15:28:58 +0545

    changed for noty

--- view/contact.php
+++ view/contact.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Pratik
+ * User: Sujan
  * Date: 12/6/2016
  * Time: 4:12 PM
  */
@@ -15,10 +15,11 @@
     <title>National Innovation Center</title>
     <link rel="icon" href="../img/logo.png" type="image/png" sizes="16x16">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="../js/jquery.noty.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="../css/style.css"/>
     <link rel="stylesheet" href="../bootstrap-3.3.6-dist/css/bootstrap.min.css"/>
-
+    <script src="../js/jquery.noty.packaged.min.js"></script>
     <!--[if lt IE 7]>
     <style type="text/css">
         #wrapper {
@@ -26,6 +27,27 @@
         }
     </style>
     <![endif]-->
+    <script>
+        <?php
+        if(isset($_SESSION["message"])){
+        $message = $_SESSION["message"];
+        $messageType = $_SESSION["messageType"];
+        ?>
+        alert("Hello");
+        notyMessage(<?php echo $message ?>,<?php echo $messageType ?>);
+        <?php
+        unset($_SESSION["message"]);
+        }
+        ?>
+        function notyMessage(message,messageType) {
+            noty({ text: message});
+            noty({
+             text: message,
+             layout: 'topRight',
+             type: messageType
+             });
+        }
+    </script>
 </head>
 <body>
 
