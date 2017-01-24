d999f26b8c47983fd06f19b0da9f1b957b7613e3
sujan0238 <sujan.chauhan@deerwalk.edu.np>
2017-01-24 15:19:55 +0545

    added time with seperate column

--- controller/c_addevents.php
+++ controller/c_addevents.php
@@ -13,7 +13,8 @@ $title = $_POST['title'];
 $date = $_POST['date'];
 $venue = $_POST['venue'];
 $desc = $_POST['description'];
+$time = $_POST['time'];
 
-addEvent($title, $date, $venue, $desc, $conn);
+addEvent($title, $date, $venue, $desc, $time, $conn);
 
 header('Location: ../view/events.php');
